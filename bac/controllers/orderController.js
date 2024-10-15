5556aae601c1cebc41b11a04a2a45c9ccf0ac172
sagar2000000 <sagar171joshi@gmail.com>
2024-10-15 09:10:44 +0545

    all code added

---
+++ bac/controllers/orderController.js
@@ -0,0 +1,14 @@
+import orderModel from "../models/orderModel.js";
+import UserModel from "../models/userModel.js";
+import stripe from "stripe";
+
+// place user order for frontend
+
+const placeOrder =async (req,res)=>{
+  
+}
+
+
+
+
+export {placeOrder}
